8b95d34ecb5ff49222024602b65e0067d1c3c9da
Ous Bayaa <bayaaous@gmail.com>
2020-03-09 18:18:02 -0700

    set up state/object array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const toDo = [{
+    make: 'do a toDo list',
+    id: 987,
+    done: false
+},
+]
